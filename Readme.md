32c693c9185fe63bb7ee8ad434ee038c9b635e70
gl_higaisha@yahoo.co.jp <gl_higaisha@yahoo.co.jp>
2021-09-10 21:05:02 +0900

    feat: first commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Install
+
+```
+pip install locust
+```
+
+# コマンド実行
+
+locust
+
+# ブラウザで確認
+
+<http://localhost:8089/>
---
+++ locustfile.py
@@ -0,0 +1,9 @@
+from locust import HttpUser, task, between
+import random
+
+class User(HttpUser):
+    wait_time = between(1, 1)
+
+    @task
+    def limit(self):
+        self.client.get("/aiakos1/reserve/limit")
